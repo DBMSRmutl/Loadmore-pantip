6c467b27cfd1cbcbe24af8fe65e284fbad643d24
DBMSRmutl <dbms.rmutl@gmail.com>
2016-01-23 14:45:14 +0700

    Create db_pantip.sql

---
+++ db_pantip.sql
@@ -0,0 +1,51 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 23, 2016 at 08:21 AM
+-- Server version: 10.1.9-MariaDB
+-- PHP Version: 7.0.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pantip`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `db_pantip`
+--
+
+CREATE TABLE `db_pantip` (
+  `id` int(11) NOT NULL,
+  `title` varchar(400) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `link` varchar(200) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `post_by` varchar(200) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `time_stamp` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `number_of_comments` int(11) DEFAULT NULL,
+  `tags` varchar(300) COLLATE utf8mb4_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `db_pantip`
+--
+ALTER TABLE `db_pantip`
+  ADD PRIMARY KEY (`id`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
